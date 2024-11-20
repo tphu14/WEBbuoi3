5d951a3899237b4c42dff8d457dc074e56d9bb08
TranPhu <nguyentranphu1402x0602@gmail.com>
2024-11-20 23:00:33 +0700

    Add project files.

---
+++ WebApplication3/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+public class StudentController : Controller
+{
+    
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult ShowKQ(string MSSV, string HoTen, float DiemTB, string ChuyenNganh)
+    {
+        
+        var sinhVienDangKy = new[]
+        {
+            new { MSSV = "001", HoTen = "Nguyen Van A", ChuyenNganh = "CNPM" },
+            new { MSSV = "002", HoTen = "Le Thi B", ChuyenNganh = "HTTT" }
+        };
+
+   
+        int soLuong = sinhVienDangKy.Count(sv => sv.ChuyenNganh == ChuyenNganh);
+
+        
+        ViewBag.MSSV = MSSV;
+        ViewBag.HoTen = HoTen;
+        ViewBag.ChuyenNganh = ChuyenNganh;
+        ViewBag.SoLuong = soLuong;
+
+        return View();
+    }
+}
